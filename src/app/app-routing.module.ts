66453649de693e8daad7e6d925a597b2a67b5be4
DayannaMacay <dayanamacay@hotmail.com>
2023-11-10 09:40:35 -0500

    componentes, ventanas, rutas y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
+import { ProductosComponent } from './screens/productos/productos.component';
+import { OfertasComponent } from './screens/ofertas/ofertas.component';
+import { NosotrosComponent } from './screens/nosotros/nosotros.component';
+import { ContactosComponent } from './screens/contactos/contactos.component';
+import { Error404Component } from './screens/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"", component:HomeComponent},
+  {path:"productos", component:ProductosComponent},
+  {path:"ofertas", component:OfertasComponent},
+  {path:"nosotros&terminos-condiciones", component:NosotrosComponent},
+  {path:"contactos", component:ContactosComponent},
+
+  {path:"**", component:Error404Component}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
